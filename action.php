bc2c007172eb6b280461d635d8256f7a153bbb6a
alfiusstevanus <alfiusstevanus18@gmail.com>
2023-03-24 18:47:29 +0700

    update member

--- action.php
+++ action.php
@@ -5,7 +5,7 @@ $username = $_POST['user_name'];
 $email = $_POST['user_email'];
 $password = ($_POST['user_password']);
 
-$query = "INSERT INTO member VALUES('','$email','$username','$password','','Member','','')";
+$query = "INSERT INTO member VALUES('','$email','$username','$password','Member','','')";
 
 mysqli_query($conn, $query);
 
--- actionUpdate.php
+++ actionUpdate.php
@@ -2,9 +2,12 @@
 
 include "server/connection.php";
 $id = $_GET['user_id'];
-$name = $_POST['user_name'];
 $email = $_POST['user_email'];
-$query = "UPDATE member SET user_name = '$name', user_email = '$email' WHERE user_id = '$id'";
+$name = $_POST['user_name'];
+$kelamin = $_POST['jenis_kelamin'];
+$alamat = $_POST['user_address'];
+
+$query = "UPDATE member SET user_email = '$email', user_name = '$name', jenis_kelamin = '$kelamin', user_address = '$alamat' WHERE user_id = '$id'";
 mysqli_query($conn, $query);
 header("location:index.php");
 die();
