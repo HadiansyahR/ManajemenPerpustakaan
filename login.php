3b40a2c2f5457205f7bf1c9dd11bed93aebeeef0
queenalpha <oxcroprichvie@gmail.com>
2023-03-25 10:46:13 +0700

    test login

--- login.php
+++ login.php
@@ -60,10 +60,9 @@ if (isset($_POST['login_btn'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="icon" href="icon/home.png" type="image/png">
+    <title>Login</title>
 </head>
 
 <body>
