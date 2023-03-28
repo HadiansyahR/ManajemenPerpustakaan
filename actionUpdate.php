d52f2d4bb7ef791f5a76604b969cf408b346a970
alfiusstevanus <alfiusstevanus18@gmail.com>
2023-03-28 13:24:49 +0700

    update profil

--- actionUpdate.php
+++ actionUpdate.php
@@ -6,7 +6,7 @@ $email = $_POST['user_email'];
 $name = $_POST['user_name'];
 $photo = str_replace(' ', '_', $name) . ".jpg";
 
-$query = "UPDATE akun SET email = '$email', name = '$name', photo = '$photo' WHERE user_id = '$id'";
+$query = "UPDATE akun SET email = '$email', name = '$name', photo = '$photo' WHERE id = '$id'";
 mysqli_query($conn, $query);
-header("location:index.php");
+header("location:profil.php");
 die();
--- profil.php
+++ profil.php
@@ -1,5 +1,14 @@
 <?php
 session_start();
+include('server/connection.php');
+$id = $_SESSION['user_id'];
+$sql = "Select * from akun WHERE id = $id";
+$result = mysqli_query($conn, $sql);
+if (mysqli_num_rows($result) > 0) {
+    $row = mysqli_fetch_assoc($result);
+    $_SESSION['user_name'] = $row['name'];
+    $_SESSION['user_email'] = $row['email'];
+}
 ?>
 
 <!DOCTYPE html>
@@ -19,12 +28,16 @@ session_start();
     <div class="container form" id="block">
         <h3>PROFIL</h3>
         <br>
-        <h6>ID: <?php echo $_SESSION['user_id']; ?></h6>
-        <h6>Nama: <?php echo $_SESSION['user_name']; ?></h6>
-        <h6>Email: <?php echo $_SESSION['user_email']; ?></h6>
-        <h6>Status: <?php echo $_SESSION['user_status']; ?></h6>
+        <h6>ID: <?php echo $row['id']; ?></h6>
+        <h6>Nama: <?php echo $row['name']; ?></h6>
+        <h6>Email: <?php echo $row['email']; ?></h6>
+        <h6>Status: <?php echo $row['status']; ?></h6>
 
-        <img width="200px" src="img/<?php echo  $_SESSION['user_photo']  ?>" alt="Profil <?php echo  $_SESSION['user_name']  ?>">
+        <!-- <img width="200px" src="img/<?php echo  $row['photo']  ?>" alt="Profil <?php echo  $row['user_name']  ?>"> -->
+        <img width="200px" src="img/user.jpg" alt="Profil <?php echo  $row['name']  ?>">
+        <br><br>
+        <a class=" btn btn-success mr-10" href="updateProfil.php" role="button">Update</a>
+        <a class=" btn btn-secondary mr-10" href="user.php" role="button">Kembali</a>
     </div>
 </body>
 
