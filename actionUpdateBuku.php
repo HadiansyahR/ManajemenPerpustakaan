35ea97d69a2415721ba6aa842f3ff110e60dcd33
alfiusstevanus <alfiusstevanus18@gmail.com>
2023-03-28 10:14:52 +0700

    update nama cover

--- actionUpdateBuku.php
+++ actionUpdateBuku.php
@@ -6,8 +6,9 @@ $judul = $_POST['judul_buku'];
 $penulis = $_POST['penulis_buku'];
 $penerbit = $_POST['penerbit_buku'];
 $tahunTerbit = $_POST['tahun_terbit'];
+$coverBuku = str_replace(' ', '_', $judul) . ".jpg";
 
-$query = "UPDATE buku SET judul_buku = '$judul', penulis_buku = '$penulis', penerbit_buku = '$penerbit', tahun_terbit = '$tahunTerbit' WHERE id_buku = '$id'";
+$query = "UPDATE buku SET judul_buku = '$judul', penulis_buku = '$penulis', penerbit_buku = '$penerbit', tahun_terbit = '$tahunTerbit', cover_buku = '$coverBuku' WHERE id_buku = '$id'";
 mysqli_query($conn, $query);
 
 header("location:index.php");
