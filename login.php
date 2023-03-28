1ef1ad7ce6c13968cf301974793b63dc30a6cab2
queenalpha <oxcroprichvie@gmail.com>
2023-03-28 08:22:41 +0700

    check

--- login.php
+++ login.php
@@ -61,7 +61,7 @@ if (isset($_POST['login_btn'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="css/Login.css">
+    <link rel="stylesheet" href="css/Log-regist.css">
     <title>Login</title>
 </head>
 
