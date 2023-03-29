769ffd84ae2faa703aedc028468f6887a50ba47e
alfiusstevanus <alfiusstevanus18@gmail.com>
2023-03-29 15:29:15 +0700

    tes

--- action.php
+++ action.php
@@ -5,7 +5,7 @@ $username = $_POST['user_name'];
 $email = $_POST['user_email'];
 $password = ($_POST['user_password']);
 $photo = str_replace(' ', '_', $username) . ".jpg";
-
+// tet
 $query = "INSERT INTO akun VALUES('','$email','$username','$password','User','$photo')";
 
 mysqli_query($conn, $query);
