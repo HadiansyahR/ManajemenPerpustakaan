4bab6a187f5baeb11a6780411bedd48c3d3d2bdd
alfiusstevanus <alfiusstevanus18@gmail.com>
2023-04-02 20:01:47 +0700

    not admin handling !

--- index.php
+++ index.php
@@ -7,6 +7,10 @@ $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
   $row = mysqli_fetch_assoc($result);
 }
+if ($_SESSION['user_status'] == 'User') {
+  header('location: user.php');
+  exit;
+}
 
 if (isset($_POST['cari'])) {
   $keyword = $_POST['keyword'];
